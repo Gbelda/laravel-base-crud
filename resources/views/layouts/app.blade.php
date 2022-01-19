c9075f571d042e3477907e13b2a28ee8329d3d16
gbelda <gbelda56@gmail.com>
2022-01-19 15:31:27 +0100

    Install bootstrap + create views file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Armata&family=Open+Sans+Condensed:wght@300;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    @include('partials.header')
+
+    <main class="site_main">
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+</body>
+
+</html>
