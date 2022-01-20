75e489a53f6937fa996966c5e19d9d70ddf3406f
gbelda <gbelda56@gmail.com>
2022-01-20 17:51:27 +0100

    create user comic page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,11 +8,8 @@ $indexes = ['characters', 'comics', 'movies', 'tv', 'games', 'collectibles', 'vi
 
     <div class="additional">
         <div class="container_main d-flex justify-content-end">
-            <h6>
-                DC POWER&#8480; VISA&reg;
-            </h6>
-            <h6>
-                ADDITIONAL DC SITES
+            <h6 class="fw-bold">
+                <a href="{{ route('comics.index') }}">ADMIN</a>
             </h6>
         </div>
     </div>
@@ -20,7 +17,9 @@ $indexes = ['characters', 'comics', 'movies', 'tv', 'games', 'collectibles', 'vi
     <div class="nav">
         <div class="container_main d-flex justify-content-between align-items-center">
             <div class="logo">
-                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="">
+                </a>
             </div>
 
             <ul class="index d-flex list-unstyled align-items-center justify-content-evenly">
